a8f6ca7712e8a83b5818ef991f391de85e395c13
Jessica Paige <killzone135@gmail.com>
2023-03-03 19:53:19 +0000

    Add: update worksheet function

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def enter_survey_results():
     """
     while True: #Loop until we get valid data
         print("Please enter the survey results, seperated by a coma, for each age range from 16 to 24")
-        print("e.g: 6,7,2,4,6,9,1,9")
+        print("e.g: 1,2,3,4,5,6,7,8,9")
         data_str = input("Please enter the results: \n")
 
         results = data_str.split(',')
@@ -32,8 +32,8 @@ def validate_results(results):
     """
     try:
         [int(value) for value in results]
-        if len(results) != 8:
-            raise ValueError (f"Expected 8 results, got {len(results)}")
+        if len(results) != 9:
+            raise ValueError (f"Expected 9 results, got {len(results)}")
         for value in results:
             if (int(value) > 10 or int(value) < 0):
                 raise ValueError(f"Expected a result between 0-10, got {value}")
@@ -42,9 +42,18 @@ def validate_results(results):
         return False #return false if data is invalid
     return True 
 
+def update_spreadsheet(data):
+    """
+    Updates our spreadsheet with the latest data
+    """
+    sheet_to_update = SHEET.worksheet("survey_results")
+
+    sheet_to_update.append_row(data)
+
 def main():
     """
     Runs all our functions for the program 
     """
-    enter_survey_results()
+    data = enter_survey_results()
+    update_spreadsheet(data)
 main()
